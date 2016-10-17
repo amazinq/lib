62e4949da009bb07211abe89dd621b814e24c426
Steffen Wißmann <Steffen Wimann>
2016-10-17 08:22:47 +0200

    Basic GUI Structure

--- src/library/gui/Frame.java
+++ src/library/gui/Frame.java
@@ -12,11 +12,14 @@ public class Frame extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = -4355269320719113054L;
+	
+	public static final int WIDTH = 1600;
+	public static final int HEIGHT = 1200;
 
 	public Frame() {
 		setTitle("Project");
 		setLocation(0, 0);
-		setSize(Toolkit.getDefaultToolkit().getScreenSize());
+		setSize(WIDTH,HEIGHT);
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 	}
 }
--- src/library/gui/Panel.java
+++ src/library/gui/Panel.java
@@ -1,10 +1,43 @@
 package library.gui;
 
-import javax.swing.JPanel;
+import java.awt.GridLayout;
 
-public class Panel extends JPanel {
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JSplitPane;
+import javax.swing.JTextField;
+
+public class Panel extends JSplitPane {
 
 	public Panel() {
+		setDividerLocation(Frame.WIDTH/2);
+		JPanel leftPanel = new JPanel();
+		JPanel rightPanel = new JPanel();
 		
+		leftPanel.setLayout(new GridLayout(4, 2));
+		
+		JLabel titleLabel = new JLabel("Titel");
+		JLabel regisseurLabel = new JLabel("Regisseur");
+		JLabel genreLabel = new JLabel("Genre");
+		JLabel locationLabel = new JLabel("Location");
+		
+		JTextField titleTextField = new JTextField();
+		JTextField regisseurTextField = new JTextField();
+		JTextField genreTextField = new JTextField();
+		JTextField locationTextField = new JTextField();
+		
+		leftPanel.add(titleLabel);
+		leftPanel.add(titleTextField);
+		leftPanel.add(regisseurLabel);
+		leftPanel.add(regisseurTextField);
+		leftPanel.add(genreLabel);
+		leftPanel.add(genreTextField);
+		leftPanel.add(locationLabel);
+		leftPanel.add(locationTextField);
+		
+		
+		
+		setLeftComponent(leftPanel);
+		setRightComponent(rightPanel);
 	}
 }
